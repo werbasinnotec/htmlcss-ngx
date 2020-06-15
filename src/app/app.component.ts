b7d6019f6648f483556d9fc0883e3ff7bd0fd82e
Martin Wiesmüller <martinwiesmuller@Martins-MacBook-Pro.local>
2020-06-15 15:32:02 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  templateUrl: './app.component.pug',
+  styleUrls: [ './themecontrol.sass', './app.component.sass' ],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'htmlcss-angular';
